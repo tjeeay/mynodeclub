40df02890752ff224f0df2f3beb9990342f36823
Tee Jay <tjeeay@outlook.com>
2016-07-07 15:17:58 +0800

    update

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ let express = require('express');
 
 let app = express();
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
    res.end('Welcom to my node club.'); 
 });
 
-app.listen(3001, function () {
+app.listen(3001, ()=> {
    console.log('Server listening on port 3001.'); 
 });
